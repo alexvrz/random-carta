cf6c5cabe462c8559d00a895eb815e00607b8f36
alexvrz <alexvrz2357@gmail.com>
2021-04-16 00:24:59 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  //setInterval(() => {
+  //document.querySelector(".card").classList.add(generateRandomSuit());
+  //document.querySelector(".card").classList.remove(generateRandomSuit());
+  //document.querySelector(".card").innerHTML = generateRandomNumber();
+  //}, 1000);
+  //setInterval(() => {
+  //document.querySelector(".card").classList.add(generateRandomSuit());
+  //}, 1000);
+  //setTimeout(() => {
+  // document.querySelector(".card").classList.add(generateRandomSuit());
+  //document.querySelector(".card").classList.remove(generateRandomSuit());
+  // document.querySelector(".card").innerHTML = generateRandomNumber();
+  // }, 1000); no pude =(
+};
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let indexNumb = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumb];
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "heart", "club", "spade"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
